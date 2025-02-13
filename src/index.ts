9cf6982dc535ba8bb16d188414995e2fc32c8794
0xsonu <45717030+0xsonu@users.noreply.github.com>
2025-02-13 18:38:17 +0530

    App config added

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,35 @@
+import "dotenv/config";
+import express, { NextFunction, Request, Response } from "express";
+import cors from "cors";
+import session from "cookie-session";
+import { config } from "./config/appConfig";
+import { asyncHandler } from "./middlewares/asyncHandler";
 
+const app = express();
+const BASE_PATH = config.BASE_PATH;
 
+app.use(express.json());
+
+app.use(express.urlencoded({ extended: true }));
+
+app.use(
+  session({
+    name: "session",
+    keys: [config.SESSION_SECRET],
+    maxAge: 24 * 60 * 60 * 1000, // 24 hr
+    secure: config.NODE_ENV === "production",
+    httpOnly: true,
+    sameSite: "lax",
+  })
+);
+
+app.use(
+  cors({
+    origin: config.FRONTEND_ORIGIN,
+    credentials: true,
+  })
+);
+
+app.listen(config.PORT, async () => {
+  console.log(`Server listening on port ${config.PORT} in ${config.NODE_ENV}`);
+});
