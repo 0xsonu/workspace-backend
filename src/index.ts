592e5e86c97f1984d5a816fdae5e5905ea927e0b
0xsonu <45717030+0xsonu@users.noreply.github.com>
2025-02-15 03:06:29 +0530

    Updated Passport Strategy and complete Authentication Flow

--- src/index.ts
+++ src/index.ts
@@ -4,6 +4,8 @@ import cors from "cors";
 import session from "cookie-session";
 import { config } from "./config/appConfig";
 import authRoutes from "./routes/authRoutes";
+import connectDatabase from "./config/databaseConfig";
+import "./config/passportConfig";
 
 const app = express();
 const BASE_PATH = config.BASE_PATH;
@@ -34,4 +36,5 @@ app.use(`${BASE_PATH}/auth`, authRoutes);
 
 app.listen(config.PORT, async () => {
   console.log(`Server listening on port ${config.PORT} in ${config.NODE_ENV}`);
+  await connectDatabase();
 });
